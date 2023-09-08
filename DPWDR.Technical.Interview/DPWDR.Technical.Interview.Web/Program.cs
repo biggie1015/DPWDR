68b215b5e78bfd19c8f79337c81bd83c8a01ffe7
Deploy <LearnDeploy@hotmail.com>
2023-09-08 02:58:51 -0400

    Added Web using MVC with Razor

---
+++ DPWDR.Technical.Interview/DPWDR.Technical.Interview.Web/Program.cs
@@ -0,0 +1,44 @@
+using DPWDR.Technical.Interview.Data.Context;
+using DPWDR.Technical.Interview.Data.Repositories;
+using DPWDR.Technical.Interview.Services.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    // Configuración de tu contexto de base de datos
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+builder.Services.AddHostedService<SchedulerService>();
+builder.Services.AddScoped<ProductRepository>();
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
